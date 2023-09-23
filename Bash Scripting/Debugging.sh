18aac1bef7c8595e8c72fd9de25b3ef2c70626a1
abliveira <dev.ab@outlook.com>
2023-09-23 01:27:48 -0300

    Create Debugging.sh

---
+++ Bash Scripting/Debugging.sh
@@ -0,0 +1,7 @@
+# There are a number of options that can be used for debugging purposes:
+
+# set -n (bash -n) just checks for syntax
+# set -x (bash -x) echos all commands after running them
+# set -v (bash -v) echos all commands before running them
+# set -u (bash -u) causes the shell to treat using unset variables as an error
+# set -e (bash -e) causes the script to exit immediately upon any non-zero exit status
